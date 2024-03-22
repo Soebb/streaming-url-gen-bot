01b06a67da77f386dfcbc0cf2e015357031a39bc
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-22 04:41:31 +0330

    Update app.py

--- app.py
+++ app.py
@@ -22,30 +22,13 @@ def homepage():
 @app.route("/yt")
 def youtube():
     try:
-        video_id = request.args['id']
+        url = request.args['id']
     except Exception as e:
         edata = "Please parse ?id= when calling the api"
         return edata
-    try:
-        encypted = request.args['en']
-    except Exception as e:
-        encypted = 1
-    if encypted == "0":
-        video_id = video_id
-    else:
-        try:
-            video_id = b64_to_str(video_id)
-        except:
-            return "<font color=red size=15>Wrong Video ID</font> <br> sorry"
-    if ("youtube.com" in video_id) and ("/" in video_id) and ("=" in video_id):
-        url = video_id
-    elif ("youtu.be" in video_id) and ("/" in video_id):
-        url = video_id
-    else:
-        vid = video_id
-        url = f"https://youtu.be/{vid}"
+
     with YoutubeDL() as ydl:
-      info_dict = ydl.extract_info(url, download=False)
+        info_dict = ydl.extract_info(url, download=False)
     video_name = info_dict['title']
     videos = [ {"format": format["height"], "url": format["url"]} for format in info_dict["formats"] if format["format_id"] in ["18", "22"] ]
     # captions = info_dict["aut|safeomatic_captions"]
